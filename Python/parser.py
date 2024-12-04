239fa6911379218ecfdd3fa7846c8ce1b21f1cef
BrenoSI03 <bresoa0@gmail.com>
2024-12-04 04:37:05 -0300

    Adicionando impressao de NUM

--- Python/parser.py
+++ Python/parser.py
@@ -1,8 +1,11 @@
 import ply.yacc as yacc
 from lexer import tokens
 
-# Regras de precedência (não é essencial aqui, mas pode ajudar com extensões)
-precedence = ()
+# Pilha para rastrear o nível de indentação
+indent_stack = [1]  # Inicia no nível 1 dentro da função main
+
+def get_indent():
+    return '    ' * indent_stack[-1]  # 4 espaços por nível
 
 # Gramática
 def p_programa(p):
@@ -28,32 +31,43 @@ def p_cmd(p):
 
 def p_atribuicao(p):
     'atribuicao : FACA VAR SER NUM PONTO'
-    p[0] = f"    int {p[2]} = {p[4]};"
+    indent = get_indent()
+    p[0] = f"{indent}int {p[2]} = {p[4]};"
 
 def p_impressao(p):
     '''impressao : MOSTRE VAR PONTO
-                 | MOSTRE NUM PONTO
-                 | MOSTRE operacao PONTO'''
-    p[0] = f"    printf(\"%d\\n\", {p[2]});"
+                 | MOSTRE operacao PONTO
+                 | MOSTRE NUM PONTO'''
+    indent = get_indent()
+    p[0] = f"{indent}printf(\"%d\\n\", {p[2]});"
 
 def p_operacao(p):
     '''operacao : SOME VAR COM VAR PONTO
                 | SOME VAR COM NUM PONTO
                 | SOME NUM COM VAR PONTO
                 | SOME NUM COM NUM PONTO'''
-    p[0] = f"{p[2]} + {p[4]}"  # Retorna a expressão para uso em impressao
+    # Retorna a expressão para uso em impressao
+    p[0] = f"{p[2]} + {p[4]}"
 
 def p_multiplique(p):
     '''cmd : MULTIPLIQUE VAR POR VAR PONTO
            | MULTIPLIQUE VAR POR NUM PONTO
            | MULTIPLIQUE NUM POR VAR PONTO
            | MULTIPLIQUE NUM POR NUM PONTO'''
-    p[0] = f"    {p[2]} *= {p[4]};"
+    indent = get_indent()
+    p[0] = f"{indent}{p[2]} *= {p[4]};"
 
 def p_repeticao(p):
     'repeticao : REPITA NUM VEZES DOIS_PONTOS cmds FIM'
-    cmds = '\n    '.join(p[6])
-    p[0] = f"    for (int i = 0; i < {p[2]}; i++) {{\n    {cmds}\n    }}"
+    indent = get_indent()
+    loop_start = f"{indent}for (int i = 0; i < {p[2]}; i++) {{"
+    
+    indent_stack.append(indent_stack[-1] + 1)  # Incrementa para o bloco interno
+    cmds = '\n'.join(p[5])
+    indent_stack.pop()  # Decrementa após o bloco
+    
+    loop_end = f"{indent}}}"
+    p[0] = f"{loop_start}\n{cmds}\n{loop_end}"
 
 def p_expressao(p):
     '''expressao : VAR
@@ -65,15 +79,38 @@ def p_se(p):
           | SE expressao ENTAO cmds SENAO cmds FIM
           | SE expressao ENTAO cmds FIM PONTO
           | SE expressao ENTAO cmds SENAO cmds FIM PONTO'''
+    indent = get_indent()
     
-    # Verifica se há uma cláusula SENAO
-    if 'SENAO' in p:
-        cmds_entao = '\n    '.join(p[4])
-        cmds_senao = '\n    '.join(p[6])
-        p[0] = f"    if ({p[2]}) {{\n    {cmds_entao}\n    }} else {{\n    {cmds_senao}\n    }}"
+    if len(p) in [6, 7] and p.slice[-1].type != 'SENAO':
+        # Forma sem SENAO
+        code = f"{indent}if ({p[2]}) {{\n"
+        indent_stack.append(indent_stack[-1] + 1)  # Incrementa o nível de indentação
+        cmds_entao = '\n'.join(p[4])
+        code += f"{cmds_entao}\n"
+        indent_stack.pop()  # Decrementa o nível de indentação
+        code += f"{indent}}}"
+        if len(p) == 7 and p.slice[-1].type == 'PONTO':
+            code += "."
+    elif 'SENAO' in p:
+        # Forma com SENAO
+        code = f"{indent}if ({p[2]}) {{\n"
+        indent_stack.append(indent_stack[-1] + 1)  # Incrementa o nível de indentação
+        cmds_entao = '\n'.join(p[4])
+        code += f"{cmds_entao}\n"
+        indent_stack.pop()  # Decrementa o nível de indentação
+        code += f"{indent}}} else {{\n"
+        indent_stack.append(indent_stack[-1] + 1)  # Incrementa o nível de indentação
+        cmds_senao = '\n'.join(p[6])
+        code += f"{cmds_senao}\n"
+        indent_stack.pop()  # Decrementa o nível de indentação
+        code += f"{indent}}}"
+        if len(p) == 8 and p.slice[-1].type == 'PONTO':
+            code += "."
     else:
-        cmds = '\n    '.join(p[4])
-        p[0] = f"    if ({p[2]}) {{\n{cmds}\n    }}"
+        # Produções não cobertas
+        code = ""
+    
+    p[0] = code
 
 def p_error(p):
     if p:
